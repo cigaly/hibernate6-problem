3186b95584f6598142a5ddfb010e4b57a4b75b04
Cedomir Igaly <cedomir.igaly@e-dama.net>
2023-05-04 11:06:09 +0200

    Test case for HHH-16498

--- src/test/java/org/example/E.java
+++ src/test/java/org/example/E.java
@@ -1,4 +1,61 @@
 package org.example;
 
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity(name = "en")
+@Table(name = "en")
 public class E {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Integer id;
+
+    @Column(nullable = true)
+    private int year;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = true)
+    private SignPosition signPosition;
+
+    @Column(nullable = true)
+    private int day;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public SignPosition getSignPosition() {
+        return signPosition;
+    }
+
+    public void setSignPosition(SignPosition signPosition) {
+        this.signPosition = signPosition;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    @Override
+    public String toString() {
+        return "E{id=%d, year=%d, signPosition=%s, day=%d}".formatted(id, year, signPosition, day);
+    }
 }
--- src/test/java/org/example/EnumValidateTest.java
+++ src/test/java/org/example/EnumValidateTest.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import jakarta.persistence.Persistence;
+import org.hibernate.Session;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class EnumValidateTest {
+
+    @Test
+    void test() {
+        final var factory = Persistence.createEntityManagerFactory("manager");
+        final var manager = factory.createEntityManager();
+        final var tx = manager.unwrap(Session.class).beginTransaction();
+
+        final var e1 = new E();
+        e1.setYear(2023);
+        e1.setSignPosition(SignPosition.BEFORE_NO_SPACE);
+        e1.setDay(25);
+        manager.persist(e1);
+        tx.commit();
+
+        final var actual = manager.find(E.class, e1.getId());
+        assertNotNull(actual);
+        assertEquals(e1.getDay(), actual.getDay());
+        assertEquals(e1.getSignPosition(), actual.getSignPosition());
+        assertEquals(e1.getYear(), actual.getYear());
+        System.out.println(actual);
+    }
 }
